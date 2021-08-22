92ea84604dd906f452589831ceb19a0932729f78
treyturley <81778542+treyturley@users.noreply.github.com>
2021-08-22 12:05:12 -0500

    Fixed syntax errors in product model.

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,15 +1,16 @@
 export class Product {
-    "id": number;
-    "name": string;
-    "description": string;
-    "price": number;
-    "imgPath": string;
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgPath: string;
 
-    constructor(){
-        this.id=0;
-        this.name="";
-        this.description="";
-        this.price=0;
-        this.imgPath="";
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.description = "";
+        this.price = 0;
+        this.imgPath = "";
     }
+
 }
