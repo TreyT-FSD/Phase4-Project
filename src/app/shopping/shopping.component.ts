9ce2e7d0a39ad9c74c1400bb9da55cd0ab3a7a1e
treyturley <81778542+treyturley@users.noreply.github.com>
2021-08-22 17:11:33 -0500

    Implemented product search.

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -9,11 +9,24 @@ import { ProductService } from '../services/product.service';
 })
 export class ShoppingComponent implements OnInit {
 
-  products: Array<Product>;
+  allProducts: Array<Product> = new Array<Product>();
 
-  cart: Array<Product>;
+  products: Array<Product> = new Array<Product>();
+
+  cart: Array<Product> = new Array<Product>();
+
+  searchTerm: string = "";
 
   constructor(private _productSvc: ProductService) {
+
+    this._productSvc.getProducts().subscribe(
+      result => {
+        this.allProducts = result;
+        this.products = result;
+      },
+      error => {
+        console.log(error);
+      });
 
     let sessionCart = sessionStorage.getItem("cart");
 
@@ -24,35 +37,44 @@ export class ShoppingComponent implements OnInit {
       this.cart = new Array<Product>();
     }
 
-    //get the products from the service
-    this.products = new Array<Product>();
+  }
 
-    _productSvc.getProducts().subscribe(
+  ngOnInit(): void { }
+
+  addProductToCart(id: number): void {
+    //get the product
+    //console.log(id);
+    this._productSvc.getProductById(id).subscribe(
       result => {
-        this.products = result;
+        console.log(result);
+        this.cart.push(result);
+        sessionStorage.setItem("cart", JSON.stringify(this.cart));
       },
       error => {
         console.log(error);
       });
-
   }
 
-  ngOnInit(): void {
-  }
+  filterProducts(search: string): void {
+    //console.log(this.searchTerm);
 
-  addProductToCart(id: number): void {
-    //get the product
-    console.log(id);
-    this._productSvc.getProductById(id).subscribe(
-      result=>{
-        console.log(result);
-        this.cart.push(result);
-        sessionStorage.setItem("cart", JSON.stringify(this.cart));
-      }, 
-      error=>{
-        console.log(error);
+    let productSubset = new Array<Product>();
+
+    //if searchTerm not empty
+    if (search != "") {
+      this.allProducts.forEach(element => {
+        if (element.name.toLowerCase().includes(search.toLowerCase())) {
+          productSubset.push(element);
+        }
       });
-  }
+      //console.log(productSubset);
+      this.products = productSubset;
+    }
+    else {
+      this.searchTerm = "";
+      this.products=this.allProducts;
+    }
 
+  }
 
 }
